43c8f2db5146b4e5fc587391f19f889c9e898663
bellicose77 <16201082@uap-bd.com>
2021-02-19 15:40:38 +0600

    login page

--- App_loging/forms.py
+++ App_loging/forms.py
@@ -3,8 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class Createuser(UserCreationForm):
-    email=forms.EmailField(required=True)
+class CreateNewUser(UserCreationForm):
+    email = forms.EmailField(required=True, label="", widget=forms.TextInput(attrs={'placeholder':'Email'}))
+    username = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    password1 = forms.CharField(
+        required = True,
+        label = "",
+        widget = forms.PasswordInput(attrs={'placeholder':'Password'})
+    )
+    password2 = forms.CharField(
+        required = True,
+        label = "",
+        widget = forms.PasswordInput(attrs={'placeholder':'Password Confirmation'})
+    )
+
+
     class Meta:
         model = User
-        fields=('email','username','password1','password2')
+        fields = ('email','username','password1','password2')
--- App_loging/views.py
+++ App_loging/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render,HttpResponseRedirect
-from .forms import Createuser
+from .forms import CreateNewUser
 from django.contrib.auth import authenticate,login
 from django.urls import reverse,reverse_lazy
 # Create your views here.
 
 def signup(request):
-    form=Createuser()
+    form=CreateNewUser()
     register=False
     if request.method =='Post':
-        form=Createuser(data=request.Post)
+        form= CreateNewUser(data=request.Post)
         if form.is_valid():
-         user=form.save()
+         user= form.save()
          register=True
          pass
-    dictt={'form':form}
-    return render(request, 'App_Login/signup.html', context={'title': 'Sign up . Social', 'form': form})
+    context={'form':form}
+
+    return render(request, 'App_loging/signup.html', context)
 
 
 
